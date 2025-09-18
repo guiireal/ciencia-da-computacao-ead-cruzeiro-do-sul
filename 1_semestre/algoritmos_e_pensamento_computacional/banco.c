6c946b401ee5427758de09cbad6c18f763cdc370
guiireal <guii_real@hotmail.com>
2025-09-17 21:48:48 -0300

    wip

---
+++ 1_semestre/algoritmos_e_pensamento_computacional/banco.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <locale.h>
+#include <windows.h>
+
+int main() {
+    // Só para imprimir acentuação sem problemas
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    setlocale(LC_ALL, ".UTF-8");
+
+    int age;
+    float salary;
+
+    printf("Digite sua idade: ");
+    scanf("%d", &age);
+
+    printf("Digite seu salário: ");
+    scanf("%f", &salary);
+
+    if (age >= 18) {
+        if (salary >= 4000) {
+            printf("Crédito aprovado com limite maior!");
+        } else if (salary >= 2000) {
+            printf("Crédito aprovado com limite menor!");
+        } else {
+            printf("Crédito reprovado! Salário não compatível!");
+        }
+    } else {
+        printf("Crédito negado, você é menor de idade!");
+    }
+
+    return 0;
+}
