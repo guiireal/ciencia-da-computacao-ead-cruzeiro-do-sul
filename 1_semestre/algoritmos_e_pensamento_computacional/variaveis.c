5573f7b7556d2e617710803e8d9fdfddc60cbe02
guiireal <guii_real@hotmail.com>
2025-09-20 18:17:33 -0300

    wip

---
+++ 1_semestre/algoritmos_e_pensamento_computacional/variaveis.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char letra = 'A';
+    char nome[50] = "Guilherme";
+
+    int idade = 32;
+    long long ano_luz = 9000000000000000000;
+
+    float mensalidade_cruzeiro_do_sul = 179.90;
+    double pi = 3.14171281084031849108413821312432143143143143141;
+
+    printf(nome);
+
+    return 0;
+}
